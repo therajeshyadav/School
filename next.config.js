196ac688768c43ace1c5667bde35db1bd559a219
therajeshyadav <140382326+therajeshyadav@users.noreply.github.com>
2025-09-14 17:56:08 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,11 @@
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { 
     unoptimized: true,
-    domains: ['localhost']
-  },
-  output: 'standalone',   
-  typescript: {
-    ignoreBuildErrors: true,  
-  },
-  experimental: {
-    missingSuspenseWithCSRBailout: false, 
+    domains: ['localhost', 'school-sepia-sigma.vercel.app']
   },
 };
 
